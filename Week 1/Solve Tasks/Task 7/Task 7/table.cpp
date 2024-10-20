670f7712ff62e5f9c26c6c26437ba70c544a8c9e
BilyalovE <eldar2117@gmail.com>
2024-10-20 21:50:01 +0300

    Task 7

--- Week 1/Solve Tasks/Task 7/Task 7/table.cpp
+++ Week 1/Solve Tasks/Task 7/Task 7/table.cpp
@@ -1,8 +1,43 @@
 #include "test_runner.h"
-
+#include <iostream>
+#include <utility>
 using namespace std;
 
-// Реализуйте здесь шаблонный класс Table
+template <typename  T>
+class Table {
+public:
+    Table(const size_t num_rows_, const size_t num_columns_)
+    {
+        Resize(num_rows_, num_columns_);
+    }
+    
+    vector<T>& operator[](const size_t index) {
+        return table[index];
+    }
+    
+    const vector<T>& operator[](const size_t index) const {
+        return table[index];
+    }
+    
+    void Resize(const size_t new_num_rows_,
+                const size_t new_num_columns_){
+        num_rows = new_num_rows_;
+        num_columns = new_num_columns_;
+        const T default_value = 1;
+        table.resize(num_rows);
+        for (auto& row : table){
+            row.resize(num_columns, default_value);
+        }
+    };
+    auto Size() const{
+        return make_pair(num_rows, num_columns);
+    }
+    
+private:
+    
+    size_t num_rows = 0, num_columns = 0;
+    vector <vector<T>> table;
+};
 
 void TestTable() {
   Table<int> t(1, 1);
