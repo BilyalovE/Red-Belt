7ee87ecb14767a02b6e27ea42530cbeb409ee391
BilyalovE <eldar2117@gmail.com>
2024-10-30 01:07:50 +0300

    Task 5

--- Week 2/Solve tasks/Task 5/Task 5/Example.cpp
+++ Week 2/Solve tasks/Task 5/Task 5/Example.cpp
@@ -4,107 +4,117 @@
 #include <queue>
 #include "profailer.h"
 #include "test_runner.h"
+#include <string>
+#include <cstdint>
+#include <map>
+#include <queue>
 
 using namespace std;
 
-
-class HotelManager {
+class HotelManager{
 public:
-  void Book(int64_t time, const string& hotel_name,
-            int client_id, int room_count) {
-    current_time_ = time;
-    hotels_[hotel_name].Book({time, client_id, room_count});
-  }
-  int ComputeClientCount(const string& hotel_name) {
-    return hotels_[hotel_name].ComputeClientCount(current_time_);
-  }
-  int ComputeRoomCount(const string& hotel_name) {
-    return hotels_[hotel_name].ComputeRoomCount(current_time_);
-  }
 
+    void Book(const int64_t time, const string& hotel_name,
+              const int client_id, const int room_count){
+        current_time = time;
+        hotel_book[hotel_name].Book({time, client_id, room_count});
+    }
+    
+    int Clients(const string& hotel_name){
+        return hotel_book[hotel_name].Clients();
+    }
+    
+    int Rooms(const string& hotel_name){
+        return hotel_book[hotel_name].Rooms();
+    }
+    
 private:
-  struct Booking {
-    int64_t time;
-    int client_id;
-    int room_count;
-  };
+    struct ClientsInfo{
+        int64_t time_m;
+        int client_id_m;
+        int room_count_m;
+    };
+    
+    static int64_t current_time;
+    
+    class HotelInfo {
+    public:
+        void Book(const ClientsInfo& client_info){
+            ++cliet_book_num[client_info.client_id_m];
+            total_book_rooms += client_info.room_count_m;
+            clients_queue.push(client_info);
+        }
+        
+        int Clients(){
+            ToolHotel();
+            return static_cast<int>(cliet_book_num.size());
+        }
+        
+        int Rooms(){
+            ToolHotel();
+            return total_book_rooms;
+        }
 
-  class HotelInfo {
-  public:
-    void Book(const Booking& booking) {
-      last_bookings_.push(booking);
-      room_count_ += booking.room_count;
-      ++client_booking_counts_[booking.client_id];
-    }
-    int ComputeClientCount(int64_t current_time) {
-      RemoveOldBookings(current_time);
-      return static_cast<int>(client_booking_counts_.size());
-    }
-    int ComputeRoomCount(int64_t current_time) {
-      RemoveOldBookings(current_time);
-      return room_count_;
-    }
-  private:
-    static const int TIME_WINDOW_SIZE = 86400;
-    queue<Booking> last_bookings_;
-    int room_count_ = 0;
-    map<int, int> client_booking_counts_;
-
-    void PopBooking() {
-      const Booking& booking = last_bookings_.front();
-      room_count_ -= booking.room_count;
-      const auto client_stat_it =
-          client_booking_counts_.find(booking.client_id);
-      if (--client_stat_it->second == 0) {
-        client_booking_counts_.erase(client_stat_it);
-      }
-      last_bookings_.pop();
-    }
-    void RemoveOldBookings(int64_t current_time) {
-      while (
-          !last_bookings_.empty()
-          && last_bookings_.front().time <= current_time - TIME_WINDOW_SIZE
-      ) {
-        PopBooking();
-      }
-    }
-  };
-
-  int64_t current_time_ = 0;
-  map<string, HotelInfo> hotels_;
+    private:
+        // ключ - клиент, значение - число бронирований у каждого клиента в гостинице
+        map<int, int> cliet_book_num;
+        int total_book_rooms = 0;
+        queue<ClientsInfo> clients_queue;
+        
+        void ToolHotel(){
+            while(!clients_queue.empty()){
+                if (current_time - clients_queue.front().time_m >= time_duration ){
+                    total_book_rooms -= clients_queue.front().room_count_m;
+                    int client_id = clients_queue.front().client_id_m;
+                    --cliet_book_num[client_id];
+                    if(cliet_book_num[client_id] == 0){
+                        cliet_book_num.erase(client_id);
+                    }
+                    clients_queue.pop();
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        
+    };
+    map<string, HotelInfo> hotel_book;
+    const static int time_duration = 86400;
 };
 
+// Инициализация статической переменной
+int64_t HotelManager::current_time = 0;
+
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  
-  HotelManager manager;
-
-  int query_count;
-  cin >> query_count;
-
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-    string query_type;
-    cin >> query_type;
-
-    if (query_type == "BOOK") {
-      int64_t time;
-      cin >> time;
-      string hotel_name;
-      cin >> hotel_name;
-      int client_id, room_count;
-      cin >> client_id >> room_count;
-      manager.Book(time, hotel_name, client_id, room_count);
-    } else {
-      string hotel_name;
-      cin >> hotel_name;
-      if (query_type == "CLIENTS") {
-        cout << manager.ComputeClientCount(hotel_name) << "\n";
-      } else if (query_type == "ROOMS") {
-        cout << manager.ComputeRoomCount(hotel_name) << "\n";
-      }
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // число запросов Qmax = 10ˆ5
+    int Q = 0;
+    cin >> Q;
+    string command = "";
+    HotelManager manager;
+    while(Q > 0){
+        cin >> command;
+        int64_t time = 0;
+        string hotel_name = "";
+        int client_id = 0, room_count = 0;
+        if(command == "BOOK"){
+            cin >> time >> hotel_name >> client_id >> room_count;
+            manager.Book(time, hotel_name, client_id, room_count);
+        }
+        else if(command == "CLIENTS"){
+            cin >> hotel_name;
+            cout << manager.Clients(hotel_name) << "\n";
+        }
+        else if(command == "ROOMS"){
+            cin >> hotel_name;
+            cout << manager.Rooms(hotel_name) << "\n";
+        }
+        else{
+            cout << "неверная команда " << command << "\n";
+            continue;
+        }
+        --Q;
     }
-  }
-
-  return 0;
 }
