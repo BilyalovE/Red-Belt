4fd0a8c70687ca26732f4b2b911111685a575548
«BilyalovE» <eldar2117@gmail.com>
2024-11-07 14:42:01 +0300

    Task 2

--- Week 4/Solve Tasks/Task 2/Task 2/stack_vector.cpp
+++ Week 4/Solve Tasks/Task 2/Task 2/stack_vector.cpp
@@ -1,10 +1,11 @@
 #include "stack_vector.h"
 
 #include "test_runner.h"
-#include "profile.h"
+#include "profailer.h"
 
 #include <random>
 #include <stdexcept>
+#include <sstream>
 using namespace std;
 
 void TestConstruction() {
@@ -19,94 +20,106 @@ void TestConstruction() {
   try {
     StackVector<int, 10> u(50);
     Assert(false, "Expect invalid_argument for too large size");
-  } catch (invalid_argument&) {
+  }
+    catch (invalid_argument&) {
   } catch (...) {
     Assert(false, "Unexpected exception for too large size");
   }
 }
 
 void TestPushBack() {
-  StackVector<int, 5> v;
-  for (size_t i = 0; i < v.Capacity(); ++i) {
-    v.PushBack(i);
-  }
-
-  try {
-    v.PushBack(0);
-    Assert(false, "Expect overflow_error for PushBack in full vector");
-  } catch (overflow_error&) {
-  } catch (...) {
-    Assert(false, "Unexpected exception for PushBack in full vector");
-  }
+    StackVector<int, 5> v;
+    for (size_t i = 0; i < v.Capacity(); ++i) {
+        v.PushBack(i);
+    }
+    ostringstream array;
+    for (size_t i = 0; i < v.Capacity(); ++i) {
+        array << v[i];
+    }
+    stringstream expected_array("01234");
+    ASSERT_EQUAL(array.str(), expected_array.str());
+    try {
+        v.PushBack(0);
+        Assert(false, "Expect overflow_error for PushBack in full vector");
+    } catch (overflow_error&) {
+    } catch (...) {
+        Assert(false, "Unexpected exception for PushBack in full vector");
+    }
 }
 
 void TestPopBack() {
-  StackVector<int, 5> v;
-  for (size_t i = 1; i <= v.Capacity(); ++i) {
-    v.PushBack(i);
-  }
-  for (int i = v.Size(); i > 0; --i) {
-    ASSERT_EQUAL(v.PopBack(), i);
-  }
-
-  try {
-    v.PopBack();
-    Assert(false, "Expect underflow_error for PopBack from empty vector");
-  } catch (underflow_error&) {
-  } catch (...) {
-    Assert(false, "Unexpected exception for PopBack from empty vector");
-  }
+    StackVector<int, 5> v;
+    for (size_t i = 1; i <= v.Capacity(); ++i) {
+        v.PushBack(i);
+    }
+    ostringstream array;
+    for (size_t i = 0; i < v.Capacity(); ++i) {
+        array << v[i];
+    }
+    stringstream expected_array("12345");
+    ASSERT_EQUAL(array.str(), expected_array.str());
+    
+    for (int i = v.Size(); i > 0; --i) {
+        ASSERT_EQUAL(v.PopBack(), i);
+    }
+    try {
+        v.PopBack();
+        Assert(false, "Expect underflow_error for PopBack from empty vector");
+    } catch (underflow_error&) {
+    } catch (...) {
+        Assert(false, "Unexpected exception for PopBack from empty vector");
+    }
 }
 
 int main() {
-  {
-    TestRunner tr;
-    RUN_TEST(tr, TestConstruction);
-    RUN_TEST(tr, TestPushBack);
-    RUN_TEST(tr, TestPopBack);
-  }
-
-  cerr << "Running benchmark..." << endl;
-  const size_t max_size = 2500;
-
-  default_random_engine re;
-  uniform_int_distribution<int> value_gen(1, max_size);
-
-  vector<vector<int>> test_data(50000);
-  for (auto& cur_vec : test_data) {
-    cur_vec.resize(value_gen(re));
-    for (int& x : cur_vec) {
-      x = value_gen(re);
+    {
+        TestRunner tr;
+        RUN_TEST(tr, TestConstruction);
+        RUN_TEST(tr, TestPushBack);
+        RUN_TEST(tr, TestPopBack);
     }
-  }
 
-  {
-    LOG_DURATION("vector w/o reserve");
+    cerr << "Running benchmark..." << endl;
+    const size_t max_size = 2500;
+
+    default_random_engine re;
+    uniform_int_distribution<int> value_gen(1, max_size);
+
+    vector<vector<int>> test_data(50000);
     for (auto& cur_vec : test_data) {
-      vector<int> v;
-      for (int x : cur_vec) {
-        v.push_back(x);
-      }
+        cur_vec.resize(value_gen(re));
+        for (int& x : cur_vec) {
+            x = value_gen(re);
+        }
     }
-  }
-  {
+
+    {
+        LOG_DURATION("vector w/o reserve");
+        for (auto& cur_vec : test_data) {
+            vector<int> v;
+            for (int x : cur_vec) {
+                v.push_back(x);
+            }
+        }
+    }
+    {
     LOG_DURATION("vector with reserve");
-    for (auto& cur_vec : test_data) {
-      vector<int> v;
-      v.reserve(cur_vec.size());
-      for (int x : cur_vec) {
-        v.push_back(x);
-      }
+        for (auto& cur_vec : test_data) {
+            vector<int> v;
+            v.reserve(cur_vec.size());
+            for (int x : cur_vec) {
+                v.push_back(x);
+            }
+        }
     }
-  }
-  {
-    LOG_DURATION("StackVector");
-    for (auto& cur_vec : test_data) {
-      StackVector<int, max_size> v;
-      for (int x : cur_vec) {
-        v.PushBack(x);
-      }
+    {
+        LOG_DURATION("StackVector");
+        for (auto& cur_vec : test_data) {
+            StackVector<int, max_size> v;
+            for (int x : cur_vec) {
+                v.PushBack(x);
+            }
+        }
     }
-  }
-  cerr << "Done" << endl;
+    cerr << "Done" << endl;
 }
--- Week 4/Solve Tasks/Task 2/Task 2/stack_vector.h
+++ Week 4/Solve Tasks/Task 2/Task 2/stack_vector.h
@@ -1,27 +1,91 @@
 #pragma once
 
 #include <stdexcept>
+#include <array>
+#include <string>
+#include <sstream>
 using namespace std;
 
 template <typename T, size_t N>
 class StackVector {
 public:
-  explicit StackVector(size_t a_size = 0);
+    explicit StackVector(size_t a_size = 0){
+        if (a_size > capacity){
+            string str_error = "Expect invalid_argument for too large size";
+            throw invalid_argument(str_error);
+        }
+        else{
+            size_array = a_size;
+        }
+    }
+    typename array<T, N>::iterator GetHead(){
+        return stackVector.begin();
+    }
+    
+    T& operator[](size_t index){
+        if(index < size_array){
+            return stackVector[index];
+        }
+        else{
+            stringstream str_error("Going beyond the boundaries of the vector");
+            throw range_error(str_error.str());
+        }
 
-  T& operator[](size_t index);
-  const T& operator[](size_t index) const;
+    }
+    const T& operator[](size_t index) const{
+        if(index < size_array){
+            return stackVector[index];
+        }
+        else{
+            stringstream str_error("Going beyond the boundaries of the vector");
+            throw range_error(str_error.str());
+        }
+    }
+    typename array<T, N>::iterator begin(){
+        return stackVector.begin();
+    }
+    typename array<T, N>::iterator end(){
+        return stackVector.begin() + size_array;
+    }
+    const typename array<T, N>::iterator begin() const {
+        return stackVector.begin();
+    }
+    const typename array<T, N>::iterator end() const {
+        return stackVector.begin() + size_array;
+    }
 
-  ??? begin();
-  ??? end();
-  ??? begin() const;
-  ??? end() const;
+    size_t Size() const {
+        return size_array;
+    }
+    size_t Capacity() const{
+        return capacity;
+    }
 
-  size_t Size() const;
-  size_t Capacity() const;
-
-  void PushBack(const T& value);
-  T PopBack();
+    void PushBack(const T& value){
+        ++size_array;
+        if(size_array <= capacity){
+            stackVector[size_array - 1] = value;
+        }
+        else{
+            string str_error = "Expect overflow_error for PushBack in full vector";
+            throw overflow_error(str_error);
+        }
+    }
+    
+    T PopBack(){
+        if(size_array != 0){
+            --size_array;
+            return stackVector[size_array];
+        }
+        else{
+            stringstream str_error("Expect underflow_error for PopBack from empty vector");
+            throw underflow_error(str_error.str());
+        }
+    }
 
 private:
+    const size_t capacity = N;
+    size_t size_array;
+    array<T, N> stackVector;
 };
 
